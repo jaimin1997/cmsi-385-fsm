c2f530170e07771e5012bddf2e9c684c3bf37ea6
Masahji Stewart <masahji@synctree.com>
2020-02-12 18:38:43 -0800

    solution for part 1

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -3,7 +3,9 @@ export default class DeterministicFiniteStateMachine {
   /**
    */
   constructor({ transitions, startState, acceptStates }) {
-    throw 'IMPLEMENT constructor(description)';
+    this.transitions = transitions;
+    this.startState = startState;
+    this.acceptStates = acceptStates;
   }
 
   /**
@@ -11,11 +13,32 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
-    throw 'IMPLEMENT transition(state, symbol)';
+    return this.transitions[state][symbol];
   }
 
-  accepts(string, state = this._startState) {
-    throw 'IMPLEMENT FiniteStateMachine.accepts';
+  isAcceptState(state) {
+    return this.acceptStates.includes(state);
   }
+
+  accepts(string, state = this.startState) {
+    const nextState = this.transition(state, string.charAt(0));
+
+    return (string.length === 0) ? this.isAcceptState(state) :
+                                   this.accepts(string.substr(1), nextState);
+  }
+
+/*
+  accepts(string, state = this.startState) {
+    let currentString = string;
+    let currentState = state;
+
+    while(currentString.length > 0) {
+      currentState = this.transition(currentState, currentString.charAt(0));
+      currentString = currentString.substr(1);
+    }
+
+    return this.isAcceptState(currentState);
+  }
+*/
 
 }
