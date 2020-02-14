11d98a0db8da56099c5a27fa0d6f7c44e8186212
Masahji Stewart <masahji@synctree.com>
2020-02-13 17:19:05 -0800

    adding simple cross product implementation

--- src/DeterministicFiniteStateMachine.js
+++ src/DeterministicFiniteStateMachine.js
@@ -8,8 +8,20 @@ export default class DeterministicFiniteStateMachine {
     this.acceptStates = acceptStates;
   }
 
+  alphabet() {
+    const alphabet = new Set();
+    
+    for(const [state, desc] of Object.entries(this.transitions)) {
+      for(const symbol of Object.keys(desc)) {
+        alphabet.add(symbol);
+      }
+    }
+
+    return alphabet.values();
+  }
+
   states() {
-    return Object.keys(this.transitions);
+    return new Set(Object.keys(this.transitions));
   }
 
   stateAccepted(state) {
@@ -21,6 +33,7 @@ export default class DeterministicFiniteStateMachine {
    * @returns a string state name
    */
   transition(state, symbol) {
+    if(!this.transitions[state]) return;
     return this.transitions[state][symbol];
   }
 
@@ -35,22 +48,55 @@ export default class DeterministicFiniteStateMachine {
 /**
  *
  */
-export function cross(dfa1, dfa2, acceptanceCriteria = (dfa1State, dfa2State) => true) {
-//.... TODO
-  return dfa1;
+export function cross(dfa1, dfa2, accepts = (dfa1State, dfa2State) => true) {
+  const acceptStates = [];
+  const transitions = {};
+  const alphabet = new Set([...dfa1.alphabet(), ...dfa2.alphabet()]);
+
+  const newState = (state1, state2) => {
+    const stateName = `m1:${state1}xm2:${state2}`;
+    if(!transitions[stateName]) {
+      if(accepts(state1, state2)) acceptStates.push(stateName);
+      transitions[stateName] = {};
+    }
+    
+    return stateName;
+  };
+
+  const startState = newState(dfa1.startState, dfa2.startState);
+
+  for(const state1 of dfa1.states()) {
+    for(const state2 of dfa2.states()) {
+      const newStateName = newState(state1, state2);
+      transitions[newStateName] = {};
+      for(const symbol of alphabet) {
+        const nextState1 = dfa1.transition(state1, symbol);
+        const nextState2 = dfa2.transition(state2, symbol);
+
+        const nextStateName = newState(nextState1, nextState2);
+        transitions[newStateName][symbol] = nextStateName;
+      }
+    }
+  }
+
+  return new DeterministicFiniteStateMachine({
+    acceptStates,
+    startState,
+    transitions
+  });
 }
 
 export function union(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(state1) || dfa2.stateAccepted(state2));
+    (state1, state2) => dfa1.stateAccepted(state1) || dfa2.stateAccepted(state2));
 }
 
 export function intersection(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(state1) && dfa2.stateAccepted(state2));
+    (state1, state2) => dfa1.stateAccepted(state1) && dfa2.stateAccepted(state2));
 }
 
 export function minus(dfa1, dfa2) {
   return cross(dfa1, dfa2, 
-    (dfa1State, dfa2State) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
+    (state1, state2) => dfa1.stateAccepted(state1) && !dfa2.stateAccepted(state2));
 }
